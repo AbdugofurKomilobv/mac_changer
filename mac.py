34a969fc773ca7fe96fd6831b81268ad17abdf1e
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-07-30 19:09:17 +0500

    end

--- mac.py
+++ mac.py
@@ -1,17 +1,21 @@
 import subprocess
-
 import optparse
 
 parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface" , dest="interface", help="interface to change its MAC address ")
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
+(options, arguments) = parser.parse_args()
 
-parser.parse_args()
+if not options.interface:
+    options.interface = input("Interface > ")
 
+if not options.new_mac:
+    options.new_mac = input("New MAC > ")
 
-interface = raw_input(" interface > ")
+print(f"[+] Changing MAC address for {options.interface} to {options.new_mac}")
 
-new_mac = raw_input(" new mac >")
-
-
+subprocess.call(["ifconfig", options.interface, "down"])
+subprocess.call(["ifconfig", options.interface, "hw", "ether", options.new_mac])
+subprocess.call(["ifconfig", options.interface, "up"])
